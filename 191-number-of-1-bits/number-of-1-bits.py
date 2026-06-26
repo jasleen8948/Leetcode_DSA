7a6510a7251deea28c49183309915825fd94489d
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 20:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (54.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,11 +1,20 @@
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         c=0
+#         while(n>0):
+#             if(n%2==1):
+#                 c+=1
+
+#             n=n//2
+#         return c
+
+# # hamming weight is the number of 1 in a bit no.
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        c=0
-        while(n>0):
-            if(n%2==1):
-                c+=1
+        count = 0
 
-            n=n//2
-        return c
-        
-# hamming weight is the number of 1 in a bit no.
+        while n:
+            count += n & 1
+            n >>= 1
+
+        return count
