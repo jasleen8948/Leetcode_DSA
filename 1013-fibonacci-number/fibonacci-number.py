2c9ea049d94e45c6a27a0161639631efcb028bd7
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 21:06:12 +0530

    Time: 48 ms (57.06%) | Memory: 19.2 MB (17.98%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,20 @@
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n <= 1:
+#             return n
+#         return self.fib(n - 1) + self.fib(n - 2)
+
 class Solution:
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-        return self.fib(n - 1) + self.fib(n - 2)
+
+        a = 0
+        b = 1
+
+        for i in range(2, n + 1):
+            c = a + b
+            a = b
+            b = c
+
+        return b
