478cc0fa4b9fb9a80a901ee6e281bbf4ca6b8a77
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 20:59:53 +0530

    Time: 3 ms (45.51%) | Memory: 20.6 MB (42.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,23 @@
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         k = 1
+
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[k - 1]:
+#                 nums[k] = nums[i]
+#                 k += 1
+
+#         return k
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = 1
+        if len(nums) == 0:
+            return 0
 
-        for i in range(1, len(nums)):
-            if nums[i] != nums[k - 1]:
-                nums[k] = nums[i]
-                k += 1
+        i = 0
 
-        return k
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
