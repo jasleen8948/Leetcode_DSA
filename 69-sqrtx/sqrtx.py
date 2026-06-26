bd04469b753471e2d747f0d9223d7945a987e353
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 20:54:16 +0530

    Time: 4 ms (43.87%) | Memory: 19.3 MB (23.39%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,34 +1,54 @@
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x < 2:
+#             return x
+
+#         left, right = 1, x // 2
+
+#         while left <= right:
+#             mid = (left + right) // 2
+
+#             if mid * mid == x:
+#                 return mid
+#             elif mid * mid < x:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+
+#         return right
+
+# # class Solution:
+# #     def mySqrt(self, x: int) -> int:
+# #         left, right = 0, x
+# #         ans = 0
+
+# #         while left <= right:
+# #             mid = (left + right) // 2
+
+# #             if mid * mid <= x:
+# #                 ans = max(ans, mid)
+# #                 left = mid + 1
+# #             else:
+# #                 right = mid - 1
+
+# #         return ans
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
 
-        left, right = 1, x // 2
+        left = 1
+        right = x // 2
+        ans = 0
 
         while left <= right:
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2
 
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
+            if mid * mid <= x:
+                ans = mid
                 left = mid + 1
             else:
                 right = mid - 1
 
-        return right
-
-# class Solution:
-#     def mySqrt(self, x: int) -> int:
-#         left, right = 0, x
-#         ans = 0
-
-#         while left <= right:
-#             mid = (left + right) // 2
-
-#             if mid * mid <= x:
-#                 ans = max(ans, mid)
-#                 left = mid + 1
-#             else:
-#                 right = mid - 1
-
-#         return ans
+        return ans
