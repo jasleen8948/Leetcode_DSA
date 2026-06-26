12708a96862552037c1e0507f133b91852782aa0
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 21:06:55 +0530

    Time: 7 ms (88.06%) | Memory: 31.3 MB (71.97%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,16 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         d = {}
+
+#         for num in nums:
+#             if num in d:
+#                 return True
+#             d[num] = 1
+
+#         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+        
