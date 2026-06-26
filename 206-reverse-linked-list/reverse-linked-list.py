a55ffdb8c2f000289c4c206900ca44f3c8c97420
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-26 21:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (64.86%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,21 +1,35 @@
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         arr = []
+
+#         current = head
+
+#         # Put all nodes in array
+#         while current:
+#             arr.append(current)
+#             current = current.next
+
+#         # Reverse connections
+#         for i in range(len(arr) - 1, 0, -1):
+#             arr[i].next = arr[i - 1]
+
+#         # Old head becomes last node
+#         if arr:
+#             arr[0].next = None
+#             return arr[-1]
+
+#         return None
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
+        prev = None
+        curr = head
 
-        current = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
 
-        # Put all nodes in array
-        while current:
-            arr.append(current)
-            current = current.next
-
-        # Reverse connections
-        for i in range(len(arr) - 1, 0, -1):
-            arr[i].next = arr[i - 1]
-
-        # Old head becomes last node
-        if arr:
-            arr[0].next = None
-            return arr[-1]
-
-        return None
+        return prev
