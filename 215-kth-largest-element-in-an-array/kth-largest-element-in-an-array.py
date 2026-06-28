cc714aabcbb891d38963daf11693b1561413c71c
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-28 15:03:29 +0530

    Time: 51 ms (85.66%) | Memory: 31.1 MB (38.25%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,13 +1,17 @@
-import heapq
+# import heapq
 
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         heap = []
+
+#         for num in nums:
+#             heapq.heappush(heap, num)
+
+#             if len(heap) > k:
+#                 heapq.heappop(heap)
+
+#         return heap[0]
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-
-        for num in nums:
-            heapq.heappush(heap, num)
-
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        return heap[0]
+        nums.sort()
+        return nums[-k]
