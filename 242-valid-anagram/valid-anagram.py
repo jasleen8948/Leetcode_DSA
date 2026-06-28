615801da9dbd1d74633b78415137149f331e022f
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-28 14:57:56 +0530

    Time: 19 ms (16.20%) | Memory: 20.5 MB (9.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,23 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         freq = [0] * 26
+
+#         for i in range(len(s)):
+#             freq[ord(s[i]) - ord('a')] += 1
+#             freq[ord(t[i]) - ord('a')] -= 1
+
+#         for count in freq:
+#             if count != 0:
+#                 return False
+
+#         return True
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        freq = [0] * 26
-
-        for i in range(len(s)):
-            freq[ord(s[i]) - ord('a')] += 1
-            freq[ord(t[i]) - ord('a')] -= 1
-
-        for count in freq:
-            if count != 0:
-                return False
-
-        return True
-
-# # # class Solution:
-# # #     def isAnagram(self, s: str, t: str) -> bool:
-# # #         return sorted(s)==sorted(t)
+        return sorted(s)==sorted(t)
 
 # # class Solution:
 # #     def isAnagram(self, s: str, t: str) -> bool:
