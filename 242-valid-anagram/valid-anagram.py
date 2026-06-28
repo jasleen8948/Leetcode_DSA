37f0b85c45b04a22c547a84e98599710bd5bda9d
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-28 14:58:37 +0530

    Time: 27 ms (5.78%) | Memory: 20.2 MB (17.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -15,33 +15,33 @@
 
 #         return True
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
-
-# # class Solution:
-# #     def isAnagram(self, s: str, t: str) -> bool:
-# #         freq = {i: 0 for i in range(26)}
-# #         if len(s)!=len(t):
-# #             return false
-# #         for i in range(len(s)):
-# #             i+=1
-# #             # freq[s[i]-'a']++(cpp)
-# #             # freq[t[i]-'a']--
-# #             # if i=0 to 26 
-# #             # freq[i]==1
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s)==sorted(t)
 
 # class Solution:
 #     def isAnagram(self, s: str, t: str) -> bool:
-#         s_clean = ""
-#         t_clean = ""
+#         freq = {i: 0 for i in range(26)}
+#         if len(s)!=len(t):
+#             return false
+#         for i in range(len(s)):
+#             i+=1
+#             freq[s[i]-'a']++(cpp)
+#             freq[t[i]-'a']--
+#             if i=0 to 26 
+#             freq[i]==1
 
-#         for ch in s:
-#             if ch.isalnum():
-#                 s_clean += ch.lower()
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_clean = ""
+        t_clean = ""
 
-#         for ch in t:
-#             if ch.isalnum():
-#                 t_clean += ch.lower()
+        for ch in s:
+            if ch.isalnum():
+                s_clean += ch.lower()
 
-#         return sorted(s_clean) == sorted(t_clean)
+        for ch in t:
+            if ch.isalnum():
+                t_clean += ch.lower()
+
+        return sorted(s_clean) == sorted(t_clean)
