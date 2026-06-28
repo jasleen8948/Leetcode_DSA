8c899c8220b8f1655e343016b73604123a801fb8
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-28 14:59:04 +0530

    Time: 11 ms (77.07%) | Memory: 19.4 MB (75.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,47 +1,21 @@
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
-
-#         freq = [0] * 26
-
-#         for i in range(len(s)):
-#             freq[ord(s[i]) - ord('a')] += 1
-#             freq[ord(t[i]) - ord('a')] -= 1
-
-#         for count in freq:
-#             if count != 0:
-#                 return False
-
-#         return True
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         return sorted(s)==sorted(t)
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         freq = {i: 0 for i in range(26)}
-#         if len(s)!=len(t):
-#             return false
-#         for i in range(len(s)):
-#             i+=1
-#             freq[s[i]-'a']++(cpp)
-#             freq[t[i]-'a']--
-#             if i=0 to 26 
-#             freq[i]==1
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_clean = ""
-        t_clean = ""
+
+        if len(s) != len(t):
+            return False
+
+        freq = {}
 
         for ch in s:
-            if ch.isalnum():
-                s_clean += ch.lower()
+            freq[ch] = freq.get(ch, 0) + 1
 
         for ch in t:
-            if ch.isalnum():
-                t_clean += ch.lower()
+            if ch not in freq:
+                return False
 
-        return sorted(s_clean) == sorted(t_clean)
+            freq[ch] -= 1
+
+            if freq[ch] < 0:
+                return False
+
+        return True
