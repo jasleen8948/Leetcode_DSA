60a261d48fd1032c551667518fa3a65a76f84b8d
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-06-29 17:50:49 +0530

    Time: 87 ms (23.34%) | Memory: 19.8 MB (6.60%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,8 +1,23 @@
+# class Solution:
+#     def firstUniqChar(self, s: str) -> int:
+
+#         for i in range(len(s)):
+#             if s.count(s[i]) == 1:
+#                 return i
+
+#         return -1
 class Solution:
     def firstUniqChar(self, s: str) -> int:
 
+        freq = {}
+
+        # Count frequency
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+
+        # Find first unique character
         for i in range(len(s)):
-            if s.count(s[i]) == 1:
+            if freq[s[i]] == 1:
                 return i
 
         return -1
