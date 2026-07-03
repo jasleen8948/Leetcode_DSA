214e168de7603a1772f000d0ca10d4cf25c1d8dd
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-03 18:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,23 +1,39 @@
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+#         rows = [set() for _ in range(9)]
+#         cols = [set() for _ in range(9)]
+#         boxes = [set() for _ in range(9)]
+
+#         for r in range(9):
+#             for c in range(9):
+#                 num = board[r][c]
+
+#                 if num == ".":
+#                     continue
+
+#                 box = (r // 3) * 3 + (c // 3)
+
+#                 if num in rows[r] or num in cols[c] or num in boxes[box]:
+#                     return False
+
+#                 rows[r].add(num)
+#                 cols[c].add(num)
+#                 boxes[box].add(num)
+
+#         return True
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
+        rows = [[False] * 9 for _ in range(9)]
+        cols = [[False] * 9 for _ in range(9)]
+        boxes = [[False] * 9 for _ in range(9)]
 
-        for r in range(9):
-            for c in range(9):
-                num = board[r][c]
-
-                if num == ".":
-                    continue
-
-                box = (r // 3) * 3 + (c // 3)
-
-                if num in rows[r] or num in cols[c] or num in boxes[box]:
-                    return False
-
-                rows[r].add(num)
-                cols[c].add(num)
-                boxes[box].add(num)
-
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    num = ord(board[i][j]) - ord('1')
+                    boxIndex = (i // 3) * 3 + (j // 3)
+                    if rows[i][num] or cols[j][num] or boxes[boxIndex][num]:
+                        return False
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = True
         return True
