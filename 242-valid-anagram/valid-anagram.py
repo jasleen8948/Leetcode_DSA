c1b9908ba9d0b85a3557c46672c8e282fdb10cdc
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-06 18:38:42 +0530

    Time: 7 ms (91.52%) | Memory: 19.3 MB (75.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -12,10 +12,10 @@
 #                 return False
 #         return True
 
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         return Counter(s)==Counter(t)
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        return Counter(s)==Counter(t)
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s)==sorted(t)
