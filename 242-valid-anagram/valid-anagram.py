3faeac43ed173e9a62f4aecb68896325917832ef
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-06 18:38:30 +0530

    Time: 15 ms (44.36%) | Memory: 20.3 MB (17.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,28 +1,5 @@
 # class Solution:
 #     def isAnagram(self, s: str, t: str) -> bool:
-
-#         if len(s) != len(t):
-#             return False
-
-#         freq = {}
-
-#         for ch in s:
-#             freq[ch] = freq.get(ch, 0) + 1
-
-#         for ch in t:
-#             if ch not in freq:
-#                 return False
-
-#             freq[ch] -= 1
-
-#             if freq[ch] < 0:
-#                 return False
-
-#         return True
-
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
 #         if len(s)!=len(t):
 #             return False
         
@@ -35,6 +12,10 @@
 #                 return False
 #         return True
 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return Counter(s)==Counter(t)
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        return sorted(s)==sorted(t)
