abda49030f96ac9ec9f9373cfebb2dd50321484e
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-06 18:32:08 +0530

    Time: 13 ms (51.38%) | Memory: 19.3 MB (75.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,34 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+
+#         if len(s) != len(t):
+#             return False
+
+#         freq = {}
+
+#         for ch in s:
+#             freq[ch] = freq.get(ch, 0) + 1
+
+#         for ch in t:
+#             if ch not in freq:
+#                 return False
+
+#             freq[ch] -= 1
+
+#             if freq[ch] < 0:
+#                 return False
+
+#         return True
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-
-        freq = {}
-
-        for ch in s:
-            freq[ch] = freq.get(ch, 0) + 1
-
-        for ch in t:
-            if ch not in freq:
+        
+        countS,countT={},{}
+        for i in range (len(s)):
+            countS[s[i]]=1 + countS.get(s[i],0)
+            countT[t[i]]=1 + countT.get(t[i],0)
+        for c in countS:
+            if countS[c]!=countT.get(c,0):
                 return False
-
-            freq[ch] -= 1
-
-            if freq[ch] < 0:
-                return False
-
         return True
