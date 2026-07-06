c63b205a441f38c77b2372cbc0a045ef397d7218
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-06 18:48:30 +0530

    Time: 1718 ms (27.44%) | Memory: 19.7 MB (94.83%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,29 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
 #         d = {}
+
 #         for i in range(len(nums)):
 #             comp = target - nums[i]
+
 #             if comp in d:
 #                 return [d[comp], i]
+
 #             d[nums[i]] = i
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         n = len(nums)
-
-#         for i in range(n):
-#             for j in range(i + 1, n):
-#                 if nums[i] + nums[j] == target:
-#                     return [i, j]
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-
-        for i in range(len(nums)):
-            comp = target - nums[i]
-
-            if comp in d:
-                return [d[comp], i]
-
-            d[nums[i]] = i
