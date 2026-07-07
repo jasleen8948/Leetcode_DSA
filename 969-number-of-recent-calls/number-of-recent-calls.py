7ff237d51a4965306359b956797c4a196f0bdae4
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-07 18:29:44 +0530

    Time: 41 ms (52.18%) | Memory: 25.1 MB (18.31%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -1,14 +1,24 @@
-from collections import deque
+# from collections import deque
 
+# class RecentCounter:
+
+#     def __init__(self):
+#         self.q = deque()
+
+#     def ping(self, t: int) -> int:
+#         self.q.append(t)
+
+#         while self.q and self.q[0] < t - 3000:
+#             self.q.popleft()
+
+#         return len(self.q)
 class RecentCounter:
-
     def __init__(self):
-        self.q = deque()
+        self.records = []
+        self.start = 0
 
     def ping(self, t: int) -> int:
-        self.q.append(t)
-
-        while self.q and self.q[0] < t - 3000:
-            self.q.popleft()
-
-        return len(self.q)
+        self.records.append(t)
+        while self.records[self.start] < t - 3000:
+            self.start += 1
+        return len(self.records) - self.start
