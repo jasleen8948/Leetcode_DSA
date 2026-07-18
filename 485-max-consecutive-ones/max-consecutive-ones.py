1b615790ded42973f6966b5c26b782a8dad80fb9
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-18 22:02:35 +0530

    Time: 4 ms (98.54%) | Memory: 21.9 MB (18.00%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -1,11 +1,24 @@
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+#         count = 0
+#         max_count = 0
+#         for num in nums:
+#             if num == 1:
+#                 count += 1
+#                 max_count = max(max_count, count)
+#             else:
+#                 count = 0
+#         return max_count
+
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        count = 0
-        max_count = 0
-        for num in nums:
-            if num == 1:
-                count += 1
-                max_count = max(max_count, count)
+        ans = cur = 0
+        for x in nums:
+            if x:
+                cur += 1
+                if cur > ans:
+                    ans = cur
             else:
-                count = 0
-        return max_count
+                cur = 0
+
+        return ans
