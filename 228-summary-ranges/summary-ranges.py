6e092f5eeaeaa9cd1aaf066ab2685343a63a2613
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-19 20:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.66%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -5,17 +5,12 @@ class Solution:
 
         while i < len(nums):
             start = nums[i]
-
             while i + 1 < len(nums) and nums[i + 1] == nums[i] + 1:
                 i += 1
-
             end = nums[i]
-
             if start == end:
                 ans.append(str(start))
             else:
                 ans.append(str(start) + "->" + str(end))
-
             i += 1
-
         return ans
