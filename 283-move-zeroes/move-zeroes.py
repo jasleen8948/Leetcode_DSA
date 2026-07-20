483f2419f45f17b2dcd5d075f2f76b85afb989ac
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-20 21:13:53 +0530

    Time: 7 ms (40.57%) | Memory: 20.5 MB (61.67%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,8 +1,16 @@
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         j = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 j += 1
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        j = 0
+        next_non_zero = 0
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                j += 1
+        for current in range(len(nums)):
+            if nums[current] != 0:
+                nums[next_non_zero], nums[current] = nums[current], nums[next_non_zero]
+                next_non_zero += 1
