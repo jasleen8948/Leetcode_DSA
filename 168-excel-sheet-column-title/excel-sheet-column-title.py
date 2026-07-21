c211814c356db553ba4aba612652be6cfef754e7
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-21 20:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.79%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -1,12 +1,8 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-
         ans = []
-
-        while columnNumber > 0:
-            
-            columnNumber -= 1
-            ans.append(chr(ord('A') + columnNumber % 26))
+        while columnNumber>0:
+            columnNumber-=1
+            ans.append(chr(ord('A')+columnNumber%26))
             columnNumber //= 26
-
         return "".join(ans[::-1])
