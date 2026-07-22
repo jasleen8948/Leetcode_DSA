bfa7861b8a13dcd79ca225babed524538fc8f1af
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-22 21:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (43.56%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,3 +1,16 @@
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         return list(set(nums1) & set(nums2))
+
+
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+        s = set(nums1)
+        ans = set()
+
+        for num in nums2:
+            if num in s:
+                ans.add(num)
+
+        return list(ans)
