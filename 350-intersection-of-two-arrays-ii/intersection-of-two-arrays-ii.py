ff90b272e7ff78e7944885a6012ee177e2b42c1e
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-25 12:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (19.07%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,20 +1,37 @@
+# class Solution:
+#     def intersect(self, nums1, nums2):
+#         freq = {}
+
+#         # Count frequency of nums1
+#         for num in nums1:
+#             if num in freq:
+#                 freq[num] += 1
+#             else:
+#                 freq[num] = 1
+
+#         ans = []
+
+#         # Find intersection
+#         for num in nums2:
+#             if num in freq and freq[num] > 0:
+#                 ans.append(num)
+#                 freq[num] -= 1
+
+#         return ans
+
+
+
+from collections import Counter
+from typing import List
+
 class Solution:
-    def intersect(self, nums1, nums2):
-        freq = {}
-
-        # Count frequency of nums1
-        for num in nums1:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] = 1
-
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count = Counter(nums1)
         ans = []
 
-        # Find intersection
         for num in nums2:
-            if num in freq and freq[num] > 0:
+            if count[num] > 0:
                 ans.append(num)
-                freq[num] -= 1
+                count[num] -= 1
 
         return ans
