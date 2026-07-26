ea500540040a390cbe2256c8368c81c123e1ea7c
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-26 16:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.89%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,7 +1,33 @@
+# class Solution:
+#     def wordPattern(self, pattern: str, s: str) -> bool:
+#         words = s.split()
+#         return (
+#             len(pattern) == len(words) and
+#             len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words))
+#         )
+
+
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         words = s.split()
-        return (
-            len(pattern) == len(words) and
-            len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words))
-        )
+
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for c, w in zip(pattern, words):
+            if c in char_to_word:
+                if char_to_word[c] != w:
+                    return False
+            else:
+                char_to_word[c] = w
+
+            if w in word_to_char:
+                if word_to_char[w] != c:
+                    return False
+            else:
+                word_to_char[w] = c
+
+        return True
