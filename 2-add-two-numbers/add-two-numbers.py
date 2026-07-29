c816f7940306b58e4603b0d077b34dd1543dd677
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-29 22:25:21 +0530

    Time: 8 ms (13.10%) | Memory: 19.3 MB (40.18%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -8,18 +8,14 @@ class Solution:
         dummy = ListNode()
         tail = dummy
         carry = 0
-
         while l1 or l2 or carry:
             if l1:
                 carry += l1.val
                 l1 = l1.next
-
             if l2:
                 carry += l2.val
                 l2 = l2.next
-
             tail.next = ListNode(carry % 10)
             tail = tail.next
             carry //= 10
-
         return dummy.next      
