bc63e3b8f71d837b525ebb568ac1ababd05ab034
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-07-30 22:15:41 +0530

    Time: 257 ms (57.38%) | Memory: 19.2 MB (91.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,44 +1,23 @@
-# class Solution:
-#     def longestPalindrome(self, s: str) -> str:
-#         if len(s) < 2:
-#             return s
-#         start = end = 0
-#         def expand(left, right):
-#             while left >= 0 and right < len(s) and s[left] == s[right]:
-#                 left -= 1
-#                 right += 1
-#             return left + 1, right - 1
-#         for i in range(len(s)):
-#             l1, r1 = expand(i, i)       # Odd length
-#             l2, r2 = expand(i, i + 1)   # Even length
-#             if r1 - l1 > end - start:
-#                 start, end = l1, r1
-#             if r2 - l2 > end - start:
-#                 start, end = l2, r2
-#         return s[start:end + 1]
-
-# BY DP
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
+        if len(s) <= 1:
+            return s
 
-        start = 0
-        max_len = 1
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-        for i in range(n):
-            dp[i][i] = True
+        max_str = s[0]
 
-        for length in range(2, n + 1):
-            for left in range(n - length + 1):
-                right = left + length - 1
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
 
-                if s[left] == s[right]:
-                    if length == 2 or dp[left + 1][right - 1]:
-                        dp[left][right] = True
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
 
-                        if length > max_len:
-                            start = left
-                            max_len = length
-
-        return s[start:start + max_len]
+        return max_str
