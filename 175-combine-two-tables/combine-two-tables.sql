9648c8031a7499e86d8f450635415daeabea2f71
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-08-01 22:10:10 +0530

    Time: 485 ms (20.92%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,8 +1,8 @@
 SELECT
-    firstName,
-    lastName,
-    city,
-    state
-FROM Person
-LEFT JOIN Address
-USING (personId);
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person AS p
+LEFT JOIN Address AS a
+ON p.personId = a.personId;
