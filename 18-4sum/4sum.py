509c587950e8d0cd13bfe186fbf6c534036c1a19
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-08-09 17:43:50 +0530

    Time: 560 ms (12.84%) | Memory: 19.3 MB (85.35%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,39 +1,65 @@
+# class Solution:
+#     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         nums.sort()
+#         n = len(nums)
+#         ans = []
+
+#         for i in range(n - 3):
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+
+#             for j in range(i + 1, n - 2):
+#                 if j > i + 1 and nums[j] == nums[j - 1]:
+#                     continue
+
+#                 left = j + 1
+#                 right = n - 1
+
+#                 while left < right:
+#                     total = nums[i] + nums[j] + nums[left] + nums[right]
+
+#                     if total == target:
+#                         ans.append([nums[i], nums[j], nums[left], nums[right]])
+
+#                         left += 1
+#                         right -= 1
+
+#                         while left < right and nums[left] == nums[left - 1]:
+#                             left += 1
+
+#                         while left < right and nums[right] == nums[right + 1]:
+#                             right -= 1
+
+#                     elif total < target:
+#                         left += 1
+
+#                     else:
+#                         right -= 1
+
+#         return ans
+
+
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         n = len(nums)
-        ans = []
+        res = set()
 
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        for i in range(n):
+            for j in range(i + 1, n):
+                l, r = j + 1, n - 1
 
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
+                while l < r:
+                    s = nums[i] + nums[j] + nums[l] + nums[r]
 
-                left = j + 1
-                right = n - 1
-
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-
-                    if total == target:
-                        ans.append([nums[i], nums[j], nums[left], nums[right]])
-
-                        left += 1
-                        right -= 1
-
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-
-                    elif total < target:
-                        left += 1
-
+                    if s == target:
+                        res.add((nums[i], nums[j], nums[l], nums[r]))
+                        l += 1
+                        r -= 1
+                    elif s < target:
+                        l += 1
                     else:
-                        right -= 1
+                        r -= 1
 
-        return ans
+        return list(map(list, res))
