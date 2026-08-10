f7bdb6e20d07aa88164f5229cc231a705a7c3b5e
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-08-10 18:38:35 +0530

    Time: 2 ms (32.52%) | Memory: 20.1 MB (56.24%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -1,12 +1,34 @@
+# class Solution:
+#     def hasPathSum(self, root, targetSum):
+#         if not root:
+#             return False
+
+#         if not root.left and not root.right:
+#             return targetSum == root.val
+
+#         return (
+#             self.hasPathSum(root.left, targetSum - root.val) or
+#             self.hasPathSum(root.right, targetSum - root.val)
+#         )
+
 class Solution:
     def hasPathSum(self, root, targetSum):
         if not root:
             return False
 
-        if not root.left and not root.right:
-            return targetSum == root.val
+        stack = [(root, root.val)]
 
-        return (
-            self.hasPathSum(root.left, targetSum - root.val) or
-            self.hasPathSum(root.right, targetSum - root.val)
-        )
+        while stack:
+            node, curr_sum = stack.pop()
+
+            if not node.left and not node.right:
+                if curr_sum == targetSum:
+                    return True
+
+            if node.right:
+                stack.append((node.right, curr_sum + node.right.val))
+
+            if node.left:
+                stack.append((node.left, curr_sum + node.left.val))
+
+        return False
