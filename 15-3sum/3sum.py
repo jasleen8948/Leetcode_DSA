3886e39aa319b5f7c920081b8bc81cc5d534a643
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-08-15 20:36:59 +0530

    Time: 487 ms (90.44%) | Memory: 22.3 MB (53.78%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,37 +1,73 @@
+# class Solution:
+#     def threeSum(self, nums):
+#         nums.sort()
+#         result = []
+
+#         for i in range(len(nums) - 2):
+
+#             # Skip duplicate first elements
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+
+#             left = i + 1
+#             right = len(nums) - 1
+
+#             while left < right:
+#                 total = nums[i] + nums[left] + nums[right]
+
+#                 if total == 0:
+#                     result.append([nums[i], nums[left], nums[right]])
+
+#                     # Skip duplicates
+#                     while left < right and nums[left] == nums[left + 1]:
+#                         left += 1
+
+#                     while left < right and nums[right] == nums[right - 1]:
+#                         right -= 1
+
+#                     left += 1
+#                     right -= 1
+
+#                 elif total < 0:
+#                     left += 1
+
+#                 else:
+#                     right -= 1
+
+#         return result
+
+
 class Solution:
     def threeSum(self, nums):
         nums.sort()
-        result = []
+        ans = []
+        n = len(nums)
 
-        for i in range(len(nums) - 2):
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break
 
-            # Skip duplicate first elements
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            left = i + 1
-            right = len(nums) - 1
+            l, r = i + 1, n - 1
 
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
 
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    # Skip duplicates
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    left += 1
-                    right -= 1
-
-                elif total < 0:
-                    left += 1
-
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
                 else:
-                    right -= 1
+                    ans.append([nums[i], nums[l], nums[r]])
 
-        return result
+                    left_val = nums[l]
+                    right_val = nums[r]
+
+                    while l < r and nums[l] == left_val:
+                        l += 1
+                    while l < r and nums[r] == right_val:
+                        r -= 1
+
+        return ans
