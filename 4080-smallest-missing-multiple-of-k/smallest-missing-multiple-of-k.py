5bf581ab14a355deb4a0f2d886dcf92030cde5a3
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-08-26 17:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (54.19%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        for i in range(1,len(nums)+2):
+            if k*i in nums:
+                continue
+            else:
+                return k*i
+
+
+        
