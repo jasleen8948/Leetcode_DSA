7cb971f7716a95d9798f1ed4a1a22c8dcd253beb
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-08 22:10:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.87%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.py
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -1,8 +1,19 @@
+# class Solution:
+#     def countCommas(self, n: int) -> int:
+#         ans = 0
+        
+#         for i in range(1, n + 1):
+#             ans += f"{i:,}".count(",")
+        
+#         return ans
+
 class Solution:
     def countCommas(self, n: int) -> int:
         ans = 0
-        
-        for i in range(1, n + 1):
-            ans += f"{i:,}".count(",")
-        
+        p = 1000
+
+        while p <= n:
+            ans += n - p + 1
+            p *= 1000
+
         return ans
