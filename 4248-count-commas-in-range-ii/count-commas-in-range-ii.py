68e1d851424449f82795095cb9f838d5e0239e90
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-09 22:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (51.70%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -1,4 +1,15 @@
+# class Solution:
+#     def countCommas(self, n: int) -> int:
+#         k = (len(str(n)) - 1) // 3 # int(log10(n)) // 3 
+#         return k * (n + 1) - (1000**(k + 1) - 1000) // 999
+
 class Solution:
     def countCommas(self, n: int) -> int:
-        k = (len(str(n)) - 1) // 3 # int(log10(n)) // 3 
-        return k * (n + 1) - (1000**(k + 1) - 1000) // 999
+        ans = 0
+        x = 1000
+
+        while x <= n:
+            ans += n - x + 1
+            x *= 1000
+
+        return ans
