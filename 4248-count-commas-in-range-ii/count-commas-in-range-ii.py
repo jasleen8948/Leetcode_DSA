de842773e2871fb351feb76312bf70ea50c83cdd
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-09 22:06:43 +0530

    Time: 1 ms (26.53%) | Memory: 19.3 MB (51.70%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -1,11 +1,4 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        ans = 0
-        p = 1000
-
-        while p <= n:
-            ans += n - p + 1
-            p *= 1000
-
-        return ans
-        
+        k = (len(str(n)) - 1) // 3 # int(log10(n)) // 3 
+        return k * (n + 1) - (1000**(k + 1) - 1000) // 999
