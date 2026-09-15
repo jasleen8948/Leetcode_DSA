ecb158e0edd14ccc58cb1b62756ddf4caa37a2a6
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-15 21:40:39 +0530

    Time: 31 ms (99.00%) | Memory: 19.3 MB (35.99%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,22 +1,31 @@
 class Solution:
     def longestPalindrome(self, s):
-        res = ""
+        # Transform string to handle odd and even length palindromes
+        t = '^#' + '#'.join(s) + '#$'
+        n = len(t)
 
-        for i in range(len(s)):
-            # Odd length palindrome
-            l = r = i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > len(res):
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
+        p = [0] * n
+        center = right = 0
 
-            # Even length palindrome
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > len(res):
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
+        for i in range(1, n - 1):
+            mirror = 2 * center - i
 
-        return res
+            if i < right:
+                p[i] = min(right - i, p[mirror])
+
+            # Expand around center
+            while t[i + (1 + p[i])] == t[i - (1 + p[i])]:
+                p[i] += 1
+
+            # Update center and right boundary
+            if i + p[i] > right:
+                center = i
+                right = i + p[i]
+
+        # Find longest palindrome
+        max_len = max(p)
+        center_index = p.index(max_len)
+
+        start = (center_index - max_len) // 2
+
+        return s[start:start + max_len]
