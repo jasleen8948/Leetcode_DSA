0b7bae52c8e159c2a210a35dd9390c6dc92aa07a
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-15 21:31:53 +0530

    Time: 1771 ms (24.19%) | Memory: 27.2 MB (6.57%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -65,84 +65,38 @@
 
 # # BY DP
 
-# class Solution:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        start = 0
+        max_len = 1
 
-#     def longestPalindrome(self, s: str) -> str:
-
-#         n = len(s)
-
-#         dp = [[False] * n for _ in range(n)]
-
-
-
-#         start = 0
-
-#         max_len = 1
-
-
-
-#         for i in range(n):
-
-#             dp[i][i] = True
-
-
-
-#         for length in range(2, n + 1):
-
-#             for left in range(n - length + 1):
-
-#                 right = left + length - 1
-
-
-
-#                 if s[left] == s[right]:
-
-#                     if length == 2 or dp[left + 1][right - 1]:
-
-#                         dp[left][right] = True
-
-
-
-#                         if length > max_len:
-
-#                             start = left
-
-#                             max_len = length
-
-
-
-#         return s[start:start + max_len]
-
-
-
+        for i in range(n):
+            dp[i][i] = True
+        for length in range(2, n + 1):
+            for left in range(n - length + 1):
+                right = left + length - 1
+                if s[left] == s[right]:
+                    if length == 2 or dp[left + 1][right - 1]:
+                        dp[left][right] = True
+                        if length > max_len:
+                            start = left
+                            max_len = length
+        return s[start:start + max_len]
 
 
 # BY BRUTE FORCE APPROACH
 
-class Solution:
-
-    def longestPalindrome(self, s: str) -> str:
-
-        if len(s) <= 1:
-
-            return s
-
-        
-
-        Max_Len=1
-
-        Max_Str=s[0]
-
-        for i in range(len(s)-1):
-
-            for j in range(i+1,len(s)):
-
-                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
-
-                    Max_Len = j-i+1
-
-                    Max_Str = s[i:j+1]
-
-
-
-        return Max_Str
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         if len(s) <= 1:
+#             return S
+#         Max_Len=1
+#         Max_Str=s[0]
+#         for i in range(len(s)-1):
+#             for j in range(i+1,len(s))
+#                 if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
+#                     Max_Len = j-i+1
+#                     Max_Str = s[i:j+1]
+#         return Max_Str
