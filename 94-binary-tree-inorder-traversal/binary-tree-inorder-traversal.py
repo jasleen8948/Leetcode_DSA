00068b912b8ef36c5b6b37ff01c3f5087fd1b01e
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-19 21:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.66%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -1,41 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-# recursive solution
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
+    def inorderTraversal(self, root):
+        result = []
 
-        def dfs(node):
-            if not node:
-                return
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                result.append(node.val)
+                inorder(node.right)
 
-            dfs(node.left)
-            ans.append(node.val)
-            dfs(node.right)
-
-        dfs(root)
-        return ans
-
-
-# iterative solution
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        stack = []
-        ans = []
-        curr = root
-
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-
-            curr = stack.pop()
-            ans.append(curr.val)
-            curr = curr.right
-
-        return ans
+        inorder(root)
+        return result
