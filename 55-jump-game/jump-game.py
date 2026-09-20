9d09c5131c75ba3fd651c4afcb5a357ce915dbe4
Jasleen Kaur <jasleen_be23cs087@srmcem.ac.in>
2026-09-20 22:09:13 +0530

    Time: 11 ms (91.73%) | Memory: 20.1 MB (85.50%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,11 +1,23 @@
+# class Solution:
+#     def canJump(self, nums):
+#         max_reach = 0
+
+#         for i in range(len(nums)):
+#             if i > max_reach:
+#                 return False
+
+#             max_reach = max(max_reach, i + nums[i])
+
+#         return True
+
 class Solution:
-    def canJump(self, nums):
-        max_reach = 0
-
-        for i in range(len(nums)):
-            if i > max_reach:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
                 return False
-
-            max_reach = max(max_reach, i + nums[i])
-
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
         return True
